d63a9800e9345e6ad2dac089aefb9b894e13274c
Jack Hallybone <7317987+jackhallybone@users.noreply.github.com>
2025-03-07 17:29:31 +0000

    mvp commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# python-create-and-print-qr-code
+# Basic Python QR Code Script
+
+A script using the [qrcode](https://pypi.org/project/qrcode/) package to create images of the right size, with optional annotations, and print them using the [brother_ql](https://pypi.org/project/brother-ql/) package.
+
+TODO: this version solves my requirements fine, but qrcode can define the size itself.
---
+++ print.py
@@ -0,0 +1,23 @@
+from brother_ql.backends.helpers import send
+from brother_ql.conversion import convert
+from brother_ql.raster import BrotherQLRaster
+
+
+def send_image_to_printer(img, paper_name):
+    """Send image to a wired Brother QL printer using `brother_ql`.
+
+    Driver setup and paper sizes: https://pypi.org/project/brother-ql/
+
+    The image size should match the paper requirements. For example,
+    38mm endless paper is called "38" and has a printable width of 413px.
+    """
+
+    qlr = BrotherQLRaster("QL-800")  # printer model
+    qlr.exception_on_warning = True
+    instructions = convert(qlr=qlr, images=[img], label=paper_name)  # images as a list
+    send(
+        instructions=instructions,
+        printer_identifier="usb://0x04f9:0x209b",  # libusb filter address
+        backend_identifier="pyusb",
+        blocking=True,
+    )
